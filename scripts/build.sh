a1fa557f744c1d86d9b906f43559864f4a35f5d5
Jon Hoare <jon.hoare@imbursepayments.com>
2018-08-16 16:40:58 +0100

    Working deployment but nodes registering with lighthouse-0.lighthouse seem flakey

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ echo "Working Directory: $PWD"
 echo ""
 
 docker build --tag cluster.sharding.apinode --file ./src/ApiNode/Dockerfile .
-docker build --tag cluster.sharding.apinode --file ./src/ClusterNode/Dockerfile .
+docker build --tag cluster.sharding.clusternode --file ./src/ClusterNode/Dockerfile .
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
 kubectl apply -f ./kubernetes/namespace.yaml
-kubectl create configmap config-postgres --from-file=config-postgres --namespace=cluster-sample
+kubectl create configmap config-postgres --from-file=config-postgres --namespace=cluster-sample --dry-run -o yaml | kubectl apply -f -
 
 kubectl apply -f ./kubernetes/lighthouse-deployment.yaml
 kubectl apply -f ./kubernetes/postgres-deployment.yaml
 kubectl apply -f ./kubernetes/apinode-deployment.yaml
+kubectl apply -f ./kubernetes/clusternode-deployment.yaml
