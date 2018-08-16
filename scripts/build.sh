f4690b442f675b0ab703f310a9350d3496f90190
Jon Hoare <jon.hoare@imbursepayments.com>
2018-08-16 09:08:42 +0100

    Local Docker Builds

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+echo "|-------- Building docker files ---------|"
+echo "Working Directory: $PWD"
+echo ""
+
+docker build --tag cluster.sharding.apinode --file ./src/ApiNode/Dockerfile .
+docker build --tag cluster.sharding.apinode --file ./src/ClusterNode/Dockerfile .
